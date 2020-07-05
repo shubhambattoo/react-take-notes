7b365dadc5c21e63ee4f20ef5422548c8e6a4b35
shubhambattoo <shubham.battoo@hotmail.com>
2020-07-05 22:56:53 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import { Header } from './components/header/Header';
 import { LeftMenu } from './components/leftMenu/LeftMenu';
 import { MainContent } from './components/mainContent/MainContent';
 import { GlobalProvider } from './context/GlobalState';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
-  return (
-    <GlobalProvider>
-      <div>
+  const component = () => {
+    return (
+      <>
         <Header />
         <main>
           <LeftMenu />
           <MainContent />
         </main>
-      </div>
-    </GlobalProvider>
+      </>
+    );
+  };
+
+  return (
+    <BrowserRouter>
+      <GlobalProvider>
+        <Switch>
+          <Route path="/app" render={component} />
+          <Route path="/" render={Home} />
+        </Switch>
+      </GlobalProvider>
+    </BrowserRouter>
   );
 }
 
